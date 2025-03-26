415267c2410d28d79d8a0a8bef6706d07767c57e
sanchxt <tpbeast007@gmail.com>
2025-03-27 00:41:15 +0530

    feat: create Link component

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -4,6 +4,7 @@ export * from './components/atoms/checkbox';
 export * from './components/atoms/divider';
 export * from './components/atoms/image';
 export * from './components/atoms/input';
+export * from './components/atoms/link';
 export * from './components/atoms/otp-input';
 export * from './components/atoms/progress';
 export * from './components/atoms/radio';
